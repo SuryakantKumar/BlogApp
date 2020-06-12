f1b803e7beaf033894d579c5b246e483419f31b3
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-13 01:51:17 +0530

    Add multiple  views

--- try_django/urls.py
+++ try_django/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import (
+    home_page,
+    about_page,
+    contact_page
+    )
 
 urlpatterns = [
-    path('', home_page),
     path('admin/', admin.site.urls),
-]
+    path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page)
+    ]
--- try_django/views.py
+++ try_django/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Welcome to Home page.</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us</h1>")
