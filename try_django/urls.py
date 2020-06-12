047c95f0dfc568d751e4fa5678f816d287a1713b
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-13 01:57:42 +0530

    Use re_path for creating urls for about page

--- try_django/urls.py
+++ try_django/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
     home_page,
@@ -25,6 +25,6 @@ from .views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
-    path('about/', about_page),
+    re_path(r'^about/$', about_page),
     path('contact/', contact_page)
     ]
