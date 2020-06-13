7bf3f6eb4ff7fa9df2bef152f0724ee85e34168d
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-13 16:32:55 +0530

    render content of text template

--- try_django/urls.py
+++ try_django/urls.py
@@ -19,12 +19,14 @@ from django.urls import path, re_path
 from .views import (
     home_page,
     about_page,
-    contact_page
-    )
+    contact_page,
+    example_page
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page),
     re_path(r'^about/$', about_page),
-    path('contact/', contact_page)
-    ]
+    path('contact/', contact_page),
+    path('example/', example_page)
+]
--- try_django/views.py
+++ try_django/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -12,3 +13,12 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "contact.html", {"title": 'Contact Us'})
+
+
+def example_page(request):
+    title = 'Example'
+    context = {"title": title}
+    template_name = "title.txt"
+    template_obj = get_template(template_name)
+    rendered_string = template_obj.render(context)
+    return render(request, "example.html", {"title": rendered_string})
