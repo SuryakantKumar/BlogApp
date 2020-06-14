5d1fc69beb481ea1b6564ba8bd93998391b57c02
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-15 02:57:40 +0530

    Detail blog post with some other db queries method

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,11 @@ from .models import BlogPost
 
 
 def blog_post_detail_page(request, slug):
+    # obj = BlogPost.objects.get(slug=slug)
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404
+    # obj = queryset.first()
     obj = get_object_or_404(BlogPost, slug=slug)
     context = {"title": obj.title, "objects": obj}
     return render(request, "blog_post_detail.html", context)
