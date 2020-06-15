abd4d667e0e3636f43277b095648b1f888978550
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-15 17:31:50 +0530

    delete post feature

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import BlogPostModelForm
 from .models import BlogPost
@@ -15,6 +15,7 @@ def blog_post_list_view(request):
     # List out objects
     # could be search
     qs = BlogPost.objects.all()
+
     template_name = 'blog/list.html'
     context = {"title": "Blog",
                "object_list": qs}
@@ -45,11 +46,13 @@ def blog_post_create_view(request):
 def blog_post_detail_view(request, slug):
     # 1 object --> Detail view
     obj = get_object_or_404(BlogPost, slug=slug)
+
     template_name = "blog/detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None, instance=obj)
@@ -62,8 +65,13 @@ def blog_post_update_view(request, slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('/blog')
+
     template_name = "blog/delete.html"
     context = {"object": obj}
     return render(request, template_name, context)
