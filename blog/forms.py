69824ca66d686feb02fdb0c8becddce6cac44ee9
Suryakant Kumar <suryakantkumar@Mac.local>
2020-06-15 17:21:00 +0530

    Update post

--- blog/forms.py
+++ blog/forms.py
@@ -15,8 +15,13 @@ class BlogPostModelForm(forms.ModelForm):
         fields = ['title', 'slug', 'content']
 
     def clean_title(self, *args, **kwargs):
+        # print(dir(self))
+        instance = self.instance
+        # print(instance)
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)     # id = instance.id
         if qs.exists():
             raise forms.ValidationError("This title is already exists. Please use a different one.")
         return title
