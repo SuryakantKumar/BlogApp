aabb496101831c8a6c95e3f4f0220a71c92d8caa
Suryakant Kumar <suryakant.kumar@mountblue.tech>
2020-06-18 13:33:50 +0530

    implement search on mutiple columns or complex lookups

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
@@ -10,6 +11,17 @@ class BlogPostQuerySet(models.QuerySet):
         now = timezone.now()
         return self.filter(publish_date__lte=now)
 
+    def search(self, query):
+        # complex lookups for different search queries
+        lookup = (Q(title__icontains=query) |
+                  Q(content__icontains=query) |
+                  Q(slug__icontains=query) |
+                  Q(user__first_name__icontains=query) |
+                  Q(user__last_name__icontains=query) |
+                  Q(user__username__icontains=query))
+
+        return self.filter(lookup)
+
 
 class BlogPostManager(models.Manager):
     def get_queryset(self):
@@ -17,6 +29,11 @@ class BlogPostManager(models.Manager):
 
     def published(self):
         return self.get_queryset().published()
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
 
 
 class BlogPost(models.Model):
--- searches/views.py
+++ searches/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import SearchQuery
+from blog.models import BlogPost
 
 
 def search_view(request):
@@ -8,7 +9,11 @@ def search_view(request):
     user = None
     if request.user.is_authenticated:
         user = request.user
+
+    context = {"query": query}
     if query is not None:
         SearchQuery.objects.create(user=user, query=query)
-    context = {"query": query}
+        blog_list = BlogPost.objects.search(query=query)
+        context['blog_list'] = blog_list
+
     return render(request, "searches/view.html", context)
