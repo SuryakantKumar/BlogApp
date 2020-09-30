b6b35b2eb2248b73c7c34c1f47580a7aca076007
Nikesh Varma <55392033+nikeshvarma@users.noreply.github.com>
2020-09-30 21:57:15 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', blog_post_list_view),
-    path('<str:slug>/', blog_post_detail_view),
-    path('<str:slug>/edit/', blog_post_update_view),
-    path('<str:slug>/delete/', blog_post_delete_view)
+    path('', blog_post_list_view, name='home'),
+    path('<str:slug>/', blog_post_detail_view, name='detail_view'),
+    path('<str:slug>/edit/', blog_post_update_view, name='update_view'),
+    path('<str:slug>/delete/', blog_post_delete_view, name='delete_view')
 ]
